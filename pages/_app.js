7793569256519a3184c5e22a6f15b385dd19155f
omarabdiwali <omarabdiwali17@gmail.com>
2023-07-11 17:30:56 -0600

    fixing favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '@/styles/globals.css'
 import { SessionProvider } from "next-auth/react"
+import Head from 'next/head'
 import { SnackbarProvider } from 'notistack'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SnackbarProvider>
       <SessionProvider session={session}>
+        <Head>
+          <link rel="shortcut icon" href="/images/favicon.ico" />
+        </Head>
         <Component {...pageProps} />
       </SessionProvider>
     </SnackbarProvider>
